dfaa8db4e1214072e93dac4b9672606e3418a5ba
Jack Transue <johnktransue@gmail.com>
2022-05-31 16:49:03 -0400

    django heroku plugin added

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -33,6 +35,8 @@ CORS_ALLOWED_ORIGINS = [
 "http://localhost:3000",
 "http://127.0.0.1:9000"
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 # Application definition
