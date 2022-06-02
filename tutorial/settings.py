9502d89b40489d9892c0116d1666d34f03cd35b7
Jack Transue <johnktransue@gmail.com>
2022-06-02 12:48:14 -0400

    admin test

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["https://bar-confessions-server.herokuapp.com/"]
 
 CORS_ALLOWED_ORIGINS = [
 "https://domain.com",
